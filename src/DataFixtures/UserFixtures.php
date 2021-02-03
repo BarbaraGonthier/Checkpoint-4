da8466297b32214014986f7f5213b44b55379c76
Barbara Gonthier <barbara.gonthier@gmail.com>
2021-02-03 15:40:29 +0100

    set userfixtures for admin

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,17 +2,32 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
+     private $passwordEncoder;
+
+     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+     {
+         $this->passwordEncoder = $passwordEncoder;
+     }
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        {
+            $admin = new User();
+            $admin->setEmail('admin@mtt.com');
+            $admin->setRoles(['ROLE_ADMIN']);
+            $admin->setPassword($this->passwordEncoder->encodePassword(
+                $admin,
+                'adminpassword'
+            ));
 
-        $manager->flush();
+            $manager->persist($admin);
+            $manager->flush();
+        }
     }
 }
