96537b43e2837e74437f4c54e22203e239599d34
Barbara Gonthier <barbara.gonthier@gmail.com>
2021-02-03 16:24:31 +0100

    added logout link

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -27,10 +28,11 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/se-deconnecter", name="app_logout", methods={"GET"})
      */
-    public function logout()
+    public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        // controller can be blank: it will never be executed!
+        throw new Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
