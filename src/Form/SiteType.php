7696301a8146ba486e6d2ed33114c2fdeafc3f52
Barbara Gonthier <barbara.gonthier@gmail.com>
2021-02-04 13:58:32 +0100

    edited sitetype

--- src/Form/SiteType.php
+++ src/Form/SiteType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Site;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,16 +13,36 @@ class SiteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('status')
-            ->add('name')
-            ->add('type')
-            ->add('place')
-            ->add('client')
-            ->add('startingDate')
-            ->add('duration')
-            ->add('quoteNumber')
-            ->add('worker')
-            ->add('comment')
+            ->add('status', TextType::class, [
+                'label' => 'Etat'
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('type', TextType::class, [
+                'label' => 'Type'
+            ])
+            ->add('place', TextType::class, [
+                'label' => 'Lieu'
+            ])
+            ->add('client', TextType::class, [
+                'label' => 'Client'
+            ])
+            ->add('startingDate', TextType::class, [
+                'label' => 'Date de début'
+            ])
+            ->add('duration', TextType::class, [
+                'label' => 'Durée estimée'
+            ])
+            ->add('quoteNumber', TextType::class, [
+                'label' => 'Numéro de devis'
+            ])
+            ->add('worker', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('comment', TextType::class, [
+                'label' => 'Commentaire'
+            ])
         ;
     }
 
