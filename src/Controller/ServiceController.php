795cf88b3b4a782a00a266ce6f24605ed3e306d9
Barbara Gonthier <barbara.gonthier@gmail.com>
2021-02-04 16:44:51 +0100

    adjusted css

---
+++ src/Controller/ServiceController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ServiceController extends AbstractController
+{
+    /**
+     * @Route("/services", name="services")
+     */
+    public function index(): Response
+    {
+        return $this->render('services/index.html.twig');
+    }
+}
